241d68a4cf131aac2189ba75faf8b8ef3dc913b4
Gabriel Mattos <gabrielmattos2003@hotmail.com>
2022-02-23 16:07:19 -0300

    finished all the app's Back-End

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :restaurants do
-  #   resources :reviews, only: [ :new, :create ]
-  # end
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
   # resources :reviews, only: [:destroy]
+  # get "restaurants", to: "restaurants#index"
+  # # get "restaurants/new"
+  # # post "restaurants"
+  # # get "restaurants/38"
+  # # get "restaurants/38/reviews/new"
+  # # post "restaurants/38/reviews"
+  # resources :restaurants
+  # resources :reviews
 end
